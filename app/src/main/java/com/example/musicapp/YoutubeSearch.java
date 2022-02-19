c016792d9ea81705714638efbc20c2730eab84ac
venya-13 <venya.vyrovoj@gmail.com>
2022-02-19 21:26:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/musicapp/YoutubeSearch.java
@@ -0,0 +1,13 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YoutubeSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_search);
+    }
+}
