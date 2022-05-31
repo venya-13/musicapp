c14dd3fa7f8df486866455adbc8d9b7ee2947a54
venya-13 <venya.vyrovoj@gmail.com>
2022-06-01 00:25:10 +0300

    manuallyChooseTrack

--- app/src/main/java/com/example/musicapp/OwnMusic.java
+++ app/src/main/java/com/example/musicapp/OwnMusic.java
@@ -95,7 +95,7 @@ public class OwnMusic extends AppCompatActivity {
 
     public void fileChooser(View view){
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
+        intent.setType("audio/*");
         startActivityForResult(intent,requestCode);
     }
 
